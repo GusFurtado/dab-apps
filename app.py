66f07374da75df98b97c5a662255a1845782af30
Gustavo Furtado <gustavofurtado2@gmail.com>
2023-01-01 19:34:05 -0300

    🔥 Remover `errorlog.log` e log handler

--- app.py
+++ app.py
@@ -1,103 +1,64 @@
-import logging
-
-from dash import (
-    Dash,
-    Input,
-    Output,
-    page_container
-)
+from dash import Dash, Input, Output, page_container
 from flask import Flask
 import plotly.io as pio
 
-from components import (
-    Dashboard,
-    Drawer,
-    DrawerSingleItem,
-    DrawerFooter,
-    Navbar
-)
-
+from components import Dashboard, Drawer, DrawerSingleItem, DrawerFooter, Navbar
 
 
 # Create server with secret key
 server = Flask(__name__)
-server.secret_key = 'SECRET_KEY'
-pio.templates.default = 'plotly_white'
-
+server.secret_key = "SECRET_KEY"
+pio.templates.default = "plotly_white"
 
 
 # Instanciate Dash app
 app = Dash(
-    name = __name__,
-    server = server,
-    title = 'My Web App',
-    use_pages = True,
-    update_title = 'Updating...',
+    name=__name__,
+    server=server,
+    title="My Web App",
+    use_pages=True,
+    update_title="Updating...",
 )
-
-
-
-# Instanciate Error Handler
-file_handler = logging.FileHandler('data/errorlog.log')
-file_handler.setLevel(logging.WARNING)
-server.logger.addHandler(file_handler)
-
 
 
 # Create Drawer
 nav_links = [
+    DrawerSingleItem(name="IBGE", icon="fa-solid:map-marker-alt", href="/ibge"),
+    DrawerSingleItem(name="IPEA", icon="icomoon-free:stats-bars", href="/ipea"),
+    DrawerSingleItem(name="Deputados", icon="map:political", href="/camara"),
     DrawerSingleItem(
-        name = 'IBGE',
-        icon = 'fa-solid:map-marker-alt',
-        href = '/ibge'
-    ),
-    DrawerSingleItem(
-        name = 'IPEA',
-        icon = 'icomoon-free:stats-bars',
-        href = '/ipea'
-    ),
-    DrawerSingleItem(
-        name = 'Deputados',
-        icon = 'map:political',
-        href = '/camara'
-    ),
-    DrawerSingleItem(
-        name = 'GitHub',
-        icon = 'akar-icons:github-fill',
-        href = 'https://github.com/GusFurtado/DadosAbertosBrasil'
+        name="GitHub",
+        icon="akar-icons:github-fill",
+        href="https://github.com/GusFurtado/DadosAbertosBrasil",
     ),
     DrawerFooter(
-        title = 'Gustavo Furtado',
-        subtitle = 'Cientista de Dados',
-        img_src = 'https://raw.githubusercontent.com/GusFurtado/MyWebsite/master/assets/profile_pic.JPG'
-    )
+        title="Gustavo Furtado",
+        subtitle="Cientista de Dados",
+        img_src="https://raw.githubusercontent.com/GusFurtado/MyWebsite/master/assets/profile_pic.JPG",
+    ),
 ]
-
 
 
 # Create Dashboard Layout
 app.layout = Dashboard(
-    children = page_container,
-    id = 'dashboard',
-    navbar = Navbar(
-        title = 'Dados Abertos Brasil',
-        id = 'dashboard-navbar'
+    children=page_container,
+    id="dashboard",
+    navbar=Navbar(title="Dados Abertos Brasil", id="dashboard-navbar"),
+    drawer=Drawer(
+        menu=nav_links,
+        logo_name="DAB",
+        logo_img="https://raw.githubusercontent.com/GusFurtado/dab_assets/main/images/favicon.ico",
     ),
-    drawer = Drawer(
-        menu = nav_links,
-        logo_name = 'DAB',
-        logo_img = 'https://raw.githubusercontent.com/GusFurtado/dab_assets/main/images/favicon.ico'
-    )
 )
 
 
-
 @app.callback(
-    Output('dashboard-navbar--title', 'children'),
-    Input('dashboard--location', 'pathname'))
+    Output("dashboard-navbar--title", "children"),
+    Input("dashboard--location", "pathname"),
+)
 def init_app(path):
     """Redirects to login page if user is not logged in.
-    
+
     Inputs
     ------
     dashboard--location.pathname
@@ -111,18 +72,14 @@ def init_app(path):
 
     try:
         return {
-            '/ibge': 'Indicadores Municipais',
-            '/ipea': 'Séries Temporais',
-            '/camara': 'Painel de Deputados'
+            "/ibge": "Indicadores Municipais",
+            "/ipea": "Séries Temporais",
+            "/camara": "Painel de Deputados",
         }[path]
     except KeyError:
-        return 'Dados Abertos Brasil'
-
+        return "Dados Abertos Brasil"
 
 
 # Run app
-if __name__ == '__main__':
-    app.run(
-        host = '0.0.0.0',
-        port = 1000
-    )
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=1000)
