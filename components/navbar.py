316584b2493eca36ce2b5f536b3e223156cc1153
GusFurtado <gustavofurtado2@gmail.com>
2024-08-15 11:03:16 -0300

    🔥 Remover arquivos desnecessários do cookiecutter

--- components/navbar.py
+++ components/navbar.py
@@ -6,14 +6,13 @@ from dash.development.base_component import Component
 from dash_iconify import DashIconify
 
 
-
 class Navbar(html.Nav):
     def __init__(
-            self,
-            title: Component,
-            children: Optional[Component] = None,
-            id: Optional[str] = None
-        ):
+        self,
+        title: Component,
+        children: Optional[Component] = None,
+        id: Optional[str] = None,
+    ):
         """Navbar components.
 
         Parameters
@@ -31,36 +30,33 @@ class Navbar(html.Nav):
             Navbar title.
         {id}--children
             Content in the right of the navbar.
-        
+
         """
 
         id = id or str(uuid.uuid4())
 
         super().__init__(
-            className = 'home-content shade0',
-            children = [
+            className="home-content shade7",
+            children=[
+                html.Span(DashIconify(icon="fa:bars"), id="open-drawer"),
                 html.Span(
-                    DashIconify(icon='fa:bars'),
-                    id = 'open-drawer'
-                ),
-                html.Span(
-                    children = [
+                    children=[
                         html.Span(
-                            className = 'text',
-                            children = title,
-                            id = f'{id}--title'
+                            className="text",
+                            children=title,
+                            id=f"{id}--title",
                         ),
                         html.Span(
-                            className = 'text',
-                            children = children,
-                            id = f'{id}--children'
-                        )
+                            className="text",
+                            children=children,
+                            id=f"{id}--children",
+                        ),
                     ],
-                    style = {
-                        'width': '100%',
-                        'display': 'flex',
-                        'justify-content': 'space-between'
-                    }
-                )
-            ]
+                    style={
+                        "width": "100%",
+                        "display": "flex",
+                        "justify-content": "space-between",
+                    },
+                ),
+            ],
         )
