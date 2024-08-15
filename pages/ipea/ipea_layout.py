5fe7d7aecc41723681fcad0dc9c3c348787daf87
GusFurtado <gustavofurtado2@gmail.com>
2024-08-15 10:43:31 -0300

    👽️ Atualizar código das séries do `ipea`

--- pages/ipea/ipea_layout.py
+++ pages/ipea/ipea_layout.py
@@ -2,57 +2,65 @@ from dash import dcc, html
 import dash_bootstrap_components as dbc
 
 
-
 class IpeaLayout(dbc.Container):
-
     INDICADORES = [
-        {'value': 'PAN_PIBPMG' , 'label': 'PIB Real'},
-        {'value': 'PAN_TDESOC' , 'label': 'Desocupação'},
-        {'value': 'PAN_IPCAG'  , 'label': 'IPCA'},
-        {'value': 'PAN_TJOVER' , 'label': 'Over/Selic'},
-        {'value': 'PAN_ERV'    , 'label': 'Dólar'},
-        {'value': 'PAN_SBC'    , 'label': 'Balança Comercial'},
-        {'value': 'PAN_DEXT'   , 'label': 'Dívida externa'}
+        {"value": "PAN4_PIBPMG4", "label": "PIB Real"},
+        {"value": "PAN_PIBCAP", "label": "PIB per Capita"},
+        {"value": "PAN4_PIBPMV4", "label": "PIB Nominal"},
+        {"value": "PAN4_FBKFI90G4", "label": "Investimento Real"},
+        {"value": "PAN4_FBKFPIBV4", "label": "Taxa de Investimento Nominal"},
+        {"value": "PAN12_QIIGG12", "label": "Produção Industrial"},
+        {"value": "PAN12_IVVRG12", "label": "Valor Real das Vendas no Varejo"},
+        {"value": "PNADC12_PO12", "label": "Pessoas Ocupadas"},
+        {"value": "PNADC12_TDESOC12", "label": "Taxa de Desocupação"},
+        {"value": "PNADC12_RRPE12", "label": "Rendimento Médio"},
+        {"value": "PAN12_IGPDIG12", "label": "IGP-DI"},
+        {"value": "PAN12_IPCAG12", "label": "IPCA"},
+        {"value": "PAN12_TJOVER12", "label": "Taxa de Juros SELIC"},
+        {"value": "PAN12_ERV12", "label": "Taxa de Câmbio Nominal"},
+        {"value": "PAN12_SBC12", "label": "Balança Comercial"},
+        {"value": "PAN12_XTV12", "label": "Exportações"},
+        {"value": "PAN12_MTV12", "label": "Importações"},
+        {"value": "PAN12_STC12", "label": "Saldo em Transações Correntes"},
+        {"value": "PAN_DEXT", "label": "Dívida Externa Total"},
+        {"value": "PAN12_DTSPY12", "label": "Dívida Pública Total"},
     ]
-    
+
     def __init__(self):
         super().__init__(
-            fluid = True,
-            children = dbc.Row([
-                dbc.Col(
-                    html.Div(
-                        dbc.Form(self.radio),
-                        className = 'coluna shadow'
+            fluid=True,
+            children=dbc.Row(
+                [
+                    dbc.Col(
+                        html.Div(dbc.Form(self.radio), className="coluna shadow"),
+                        width=12,
+                        md=3,
                     ),
-                    width = 12,
-                    md = 3
-                ),
-                dbc.Col(
-                    html.Div(
-                        dcc.Loading(
-                            dcc.Graph(
-                                id = 'main_graph'
-                            )
+                    dbc.Col(
+                        html.Div(
+                            dcc.Loading(dcc.Graph(id="main_graph")),
+                            className="coluna shadow",
                         ),
-                        className = 'coluna shadow'
+                        width=12,
+                        md=9,
                     ),
-                    width = 12,
-                    md = 9
-                )
-            ])
+                ]
+            ),
         )
 
     @property
     def radio(self) -> html.Div:
-        return html.Div([
-            dbc.Label(
-                'Indicador',
-                className = 'mt-2 mb-1 bold_label',
-                html_for = 'kpi_radio'
-            ),
-            dbc.RadioItems(
-                options = self.INDICADORES,
-                value = 'PAN_PIBPMG',
-                id = 'kpi_radio'
-            )
-        ])
+        return html.Div(
+            [
+                dbc.Label(
+                    "Indicador",
+                    className="mt-2 mb-1 bold_label",
+                    html_for="kpi_radio",
+                ),
+                dbc.RadioItems(
+                    options=self.INDICADORES,
+                    value="PAN4_PIBPMG4",
+                    id="kpi_radio",
+                ),
+            ]
+        )
--- pages/ipea/ipea_plot.py
+++ pages/ipea/ipea_plot.py
@@ -3,45 +3,30 @@ import pandas as pd
 import plotly.graph_objects as go
 
 
-
 class Serie:
-
-    def __init__(self, kpi:str):
+    def __init__(self, kpi: str):
         self.serie = ipea.Serie(kpi)
-        self.serie.dados.data = pd.to_datetime(self.serie.dados.data, utc=True).dt.year
-        self.dados = self.serie.dados.loc[self.serie.dados.data > 1994, ['data','valor']]
-
+        self.serie.dados = self.serie.dados.loc[
+            self.serie.dados.data.astype(str) >= "1995-01-01", ["data", "valor"]
+        ]
 
     def plot(self) -> go.Figure:
         fig = go.Figure(
-            data = go.Bar(
-                x = self.dados.data,
-                y = self.dados.valor,
-                name = self.serie.nome,
-                marker = {
-                    'line': {
-                        'width': 0
-                    }
-                }
+            data=go.Bar(
+                x=self.serie.dados.data,
+                y=self.serie.dados.valor,
+                name=self.serie.nome,
+                marker={"line": {"width": 0}},
             ),
-            layout = {
-                'title': {
-                    'text': f'<b>{self.serie.nome}</b>',
-                    'font': {'family': 'Montserrat'}
+            layout={
+                "title": {
+                    "text": f"<b>{self.serie.nome}</b>",
+                    "font": {"family": "Montserrat"},
                 },
-                'yaxis': {
-                    'title': self.serie.unidade
-                },
-                'xaxis': {
-                    'type': 'category'
-                },
-                'margin': {
-                    'l': 5,
-                    'r': 5,
-                    't': 50,
-                    'b': 5
-                }
-            }
+                "yaxis": {"title": self.serie.unidade},
+                "xaxis": {"type": "category"},
+                "margin": {"l": 5, "r": 5, "t": 50, "b": 5},
+            },
         )
 
         return fig
